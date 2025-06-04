015c6afa7f42c9ff169f405a3f3c24bd21b40494
Jardar <jardar.ton@bouvet.no>
2025-06-04 16:07:06 +0200

    new namespace again

--- flake.nix
+++ flake.nix
@@ -441,7 +441,7 @@
       };
       # In this section, the main thing you will need to do is change the default package name
       # to the name of the packageDefinitions entry you wish to use as the default.
-      defaultPackageName = "nixCats";
+      defaultPackageName = "catsvim";
     in
 
     # see :help nixCats.flake.outputs.exports
