331ccd20915e77e94dd62d8b045236f14ec5ae5b
Jardar <jardar.ton@bouvet.no>
2025-06-06 12:40:08 +0200

    addad minimal nvim

--- nvims.nix
+++ nvims.nix
@@ -1,65 +1,96 @@
-inputs: {
-  # These are the names of your packages
-  # you can include as many as you wish.
-  catsvim =
+inputs:
+let
+  inherit (inputs.nixCats) utils;
+  catsvim_settings =
     { pkgs, name, ... }:
     {
-      # they contain a settings set defined above
       # see :help nixCats.flake.outputs.settings
-      settings = {
-        suffix-path = true;
-        suffix-LD = true;
-        wrapRc = true;
-        # IMPORTANT:
-        # your alias may not conflict with your other packages.
-        aliases = [ "n" ];
-        neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
-      };
-      # and a set of categories that you want
-      # (and other information to pass to lua)
-      categories = {
-        general = true;
-        gitPlugins = true;
-        completion = true;
-        database = true;
-        snippets = true;
-        extras = true;
-        practice = true;
-        explorer = true;
-        test = true;
-        welcome = false;
-        undotree = true;
-        statusline = true;
-        formatlint = true;
-        debugtest = true;
-        devops = false;
-        notify = true;
-        git = true;
-
-        langs = {
-          rust = false;
-          web = true;
-          go = true;
-          markdown = true;
-          lua = true;
-          dotnet = false;
-          zig = false;
-          java = false;
-        };
-        opts = {
-          welcome = {
-            snacks = true;
-            alpha = false;
-          };
-          theme = {
-            base16 = {
-              enable = false;
-              table = { };
-            };
-            name = "kanagawa"; # "kanagawa"; # "catppuccin-gruvbox";
-          };
-        };
-      };
-
+      suffix-path = true;
+      suffix-LD = true;
+      wrapRc = true;
+      # IMPORTANT:
+      # your alias may not conflict with your other packages.
+      aliases = [ "cvim" ];
+      neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
     };
+  catsvim_categories =
+    { pkgs, ... }:
+    {
+      general = true;
+      gitPlugins = true;
+      completion = true;
+      database = true;
+      snippets = true;
+      extras = true;
+      practice = true;
+      explorer = true;
+      test = true;
+      welcome = false;
+      undotree = true;
+      statusline = true;
+      formatlint = true;
+      debugtest = true;
+      devops = false;
+      notify = true;
+      git = true;
+
+      langs = {
+        rust = false;
+        web = true;
+        go = true;
+        markdown = true;
+        lua = true;
+        dotnet = false;
+        zig = false;
+        java = false;
+      };
+      opts = {
+        welcome = {
+          snacks = true;
+          alpha = false;
+        };
+        theme = {
+          base16 = {
+            enable = false;
+            table = { };
+          };
+          name = "kanagawa"; # "kanagawa"; # "catppuccin-gruvbox";
+        };
+      };
+    };
+in
+{
+  catsvim = args: {
+    settings = catsvim_settings args // {
+    };
+    categories = catsvim_categories args // {
+    };
+  };
+  #minimal
+  catsvi = args: {
+    settings = catsvim_settings args // {
+      aliases = [ "cvi" ];
+    };
+    categories = catsvim_categories args // {
+      general = true;
+      completion = true;
+      database = false;
+      snippets = false;
+      extras = false;
+      practice = false;
+      explorer = false;
+      test = false;
+      welcome = false;
+      undotree = false;
+      statusline = false;
+      formatlint = false;
+      debugtest = false;
+      devops = false;
+      notify = false;
+      git = false;
+      langs.go = false;
+      langs.web = false;
+      langs.markdown = false;
+    };
+  };
 }
