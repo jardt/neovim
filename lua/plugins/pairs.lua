29814be014cb6ee5c7a9e794191cbddfdf2ced99
jdr <jdr@internal>
2026-02-20 11:33:40 +0100

    disable autopair

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -20,8 +20,8 @@ return {
 				-- you can call require("blink.pairs.mappings").enable()
 				-- and require("blink.pairs.mappings").disable()
 				-- to enable/disable mappings at runtime
-				enabled = true,
-				cmdline = true,
+				enabled = false,
+				cmdline = false,
 				-- or disable with `vim.g.pairs = false` (global) and `vim.b.pairs = false` (per-buffer)
 				-- and/or with `vim.g.blink_pairs = false` and `vim.b.blink_pairs = false`
 				disabled_filetypes = {},
